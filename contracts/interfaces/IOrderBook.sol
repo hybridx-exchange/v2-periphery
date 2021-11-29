759790e5e241006de81cd02d6860da90ae940f25
cherideal <101du@163.com>
2021-11-29 17:21:25 +0800

    Decoupling from orderbook function

--- contracts/interfaces/IOrderBook.sol
+++ contracts/interfaces/IOrderBook.sol
@@ -4,10 +4,10 @@ interface IOrderBook {
     function getAmountOutForMovePrice(uint amountInOffer, uint reserveIn, uint reserveOut)
     external
     view
-    returns (uint amountOutGet, uint amountInLeft, uint reserveInReturn, uint reserveOutReturn);
+    returns (uint amountOutGet, uint amountInLeft, uint reserveInRet, uint reserveOutRet);
 
     function getAmountInForMovePrice(uint amountOutOffer, uint reserveIn, uint reserveOut)
     external
     view
-    returns (uint amountInGet, uint amountOutLeft, uint reserveInReturn, uint reserveOutReturn);
+    returns (uint amountInGet, uint amountOutLeft, uint reserveInRet, uint reserveOutRet);
 }
