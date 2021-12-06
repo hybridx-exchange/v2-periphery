a4a5b0a16f36bcc5166208fc1f8243a863c103c7
cherideal <101du@163.com>
2021-12-06 20:21:17 +0800

    Decoupling from orderbook function

--- test/ExampleSlidingWindowOracle.spec.ts
+++ test/ExampleSlidingWindowOracle.spec.ts
@@ -150,14 +150,14 @@ describe('ExampleSlidingWindowOracle', () => {
     it('gas for first update (allocates empty array)', async () => {
       const tx = await slidingWindowOracle.update(token0.address, token1.address, overrides)
       const receipt = await tx.wait()
-      expect(receipt.gasUsed).to.eq('116816')
+      expect(receipt.gasUsed).to.eq('119059')
     }).retries(2) // gas test inconsistent
 
     it('gas for second update in the same period (skips)', async () => {
       await slidingWindowOracle.update(token0.address, token1.address, overrides)
       const tx = await slidingWindowOracle.update(token0.address, token1.address, overrides)
       const receipt = await tx.wait()
-      expect(receipt.gasUsed).to.eq('25574')
+      expect(receipt.gasUsed).to.eq('27870')
     }).retries(2) // gas test inconsistent
 
     it('gas for second update different period (no allocate, no skip)', async () => {
@@ -165,7 +165,7 @@ describe('ExampleSlidingWindowOracle', () => {
       await mineBlock(provider, startTime + 3600)
       const tx = await slidingWindowOracle.update(token0.address, token1.address, overrides)
       const receipt = await tx.wait()
-      expect(receipt.gasUsed).to.eq('94703')
+      expect(receipt.gasUsed).to.eq('96946')
     }).retries(2) // gas test inconsistent
 
     it('second update in one timeslot does not overwrite', async () => {
