58769762def546027daf17f4804b901d7886e80f
cherideal <101du@163.com>
2022-02-11 18:51:40 +0800

    Decoupling from orderbook function

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -18,7 +18,13 @@ library UniswapV2Library {
 
     // calculates the CREATE2 address for a pair without making any external calls
     function pairFor(address factory, address tokenA, address tokenB) internal view returns (address pair) {
-        pair = IUniswapV2Factory(factory).getPair(tokenA, tokenB);
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair = address(uint(keccak256(abi.encodePacked(
+            hex'ff',
+            factory,
+            keccak256(abi.encodePacked(token0, token1)),
+            IUniswapV2Factory(factory).getCodeHash() // init code hash
+        ))));
     }
 
     // fetches and sorts the reserves for a pair
